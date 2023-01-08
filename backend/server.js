72d996e85c1d8b71008790669efa27218d3859d5
Gabriel Cabrejas <gabriel@cabrejas.net>
2023-01-08 17:52:38 +0100

    pagination

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const express = require('express')
+const parse = require('parse-link-header');
 const { errorHandling } = require('./middleware/error_handling.js')
 const { parseGitHubURL } = require('./middleware/validation.js')
 const { authString } = require('../config.js')
@@ -26,13 +27,10 @@ server.get('/doit', parseGitHubURL, async (req, res, next) => {
     for (let header of githubResponseHeaders) {
       headersForClient.push(header)
     }
+    const linkHeader = headersForClient.find(header => header[0] === 'link')[1];
+    const parsedLinkHeader = parse(linkHeader)
     const jsonPRs = await responsePRs.json()
     const commitPromises = jsonPRs.map(pr => {
-      const headers = new Headers
-      headers.set('X-GitHub-Api-Version', '2022-11-28')
-      headers.set('Accept', 'application/vnd.github+json')
-      headers.set('Authorization', `Basic ${authString}`)
-
       const urlCommits = `https://api.github.com/repos/${user}/${repo}/pulls/${pr.number}/commits`
       return fetch(urlCommits, { headers })
     })
@@ -40,18 +38,21 @@ server.get('/doit', parseGitHubURL, async (req, res, next) => {
     const responseCommits = await Promise.all(commitPromises)
     const jsonCommits = await Promise.all(responseCommits.map(res => res.json()))
 
-    const dataForClient = jsonPRs.map((pr, idx) => {
-      return {
-        id: pr.id,
-        number: pr.number,
-        title: pr.title,
-        author: pr.user.login,
-        commit_count: jsonCommits[idx].length,
-        commits: jsonCommits[idx].map(commit => {
-          return commit.commit.message
-        }),
-      }
-    })
+    const dataForClient = {
+      links: parsedLinkHeader,
+      data: jsonPRs.map((pr, idx) => {
+        return {
+          id: pr.id,
+          number: pr.number,
+          title: pr.title,
+          author: pr.user.login,
+          commit_count: jsonCommits[idx].length,
+          commits: jsonCommits[idx].map(commit => {
+            return commit.commit.message
+          }),
+        }
+      })
+    }
     res.json(dataForClient)
   } catch (error) {
     next(error)
--- frontend/index.js
+++ frontend/index.js
@@ -1,19 +1,47 @@
 (function () {
+  const resultsPerPageSelect = document.querySelector('#resultsPerPageSelect')
+  const pageNumberSelect = document.querySelector('#pageNumberSelect')
+  const openPRForm = document.querySelector('#openPRForm')
+  const URLInput = document.querySelector('#URLInput')
+  const errorMessage = document.querySelector('#errorMessage')
+  const nextButton = document.querySelector('#nextButton')
+  const lastButton = document.querySelector('#lastButton')
+  const prevButton = document.querySelector('#prevButton')
+
+  const setClickHandler = (id, url) => {
+    const button = document.getElementById(id)
+    button.disabled = false
+    button.onclick = async () => {
+      const response = await fetch(url)
+      const json = await response.json()
+      console.log(json)
+    }
+  }
+
   const submit = async evt => {
     evt.preventDefault()
-    const limit = resultsPerPageSelect.value
-    const page = pageNumberSelect.value
+    try {
+      const limit = resultsPerPageSelect.value
+      const page = pageNumberSelect.value
 
-    const repoRaw = URLInput.value.trim()
-    const repo = encodeURIComponent(repoRaw)
+      const repoRaw = URLInput.value.trim()
+      const repo = encodeURIComponent(repoRaw)
 
-    const response = await fetch(`/doit?repo=${repo}&limit=${limit}&page=${page}`)
-    const json = await response.json()
+      const response = await fetch(`/doit?repo=${repo}&limit=${limit}&page=${page}`)
+      const json = await response.json()
+      console.log(json)
 
-    if (response.status !== 200) {
-      errorMessage.textContent = json.message
+      if (response.status !== 200) {
+        errorMessage.textContent = json.message
+      } else {
+        if (json.links.next) {
+          setClickHandler('nextButton', json.links.next.url)
+        }
+      }
+    } catch (error) {
+      console.error(error.message)
+      errorMessage.textContent = 'Something went horribly wrong... Please reload the page.'
     }
-    console.log(json)
   }
 
   openPRForm.addEventListener('submit', submit)
