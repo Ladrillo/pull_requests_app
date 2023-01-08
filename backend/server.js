8c975bd72b7acbfbc6dd53c3d6496a62e71a8041
Gabriel Cabrejas <gabriel@cabrejas.net>
2023-01-08 16:30:48 +0100

    wip

--- backend/server.js
+++ backend/server.js
@@ -17,16 +17,37 @@ server.get('/doit', parseGitHubURL, async (req, res, next) => {
 
     const { repo, user } = req.repoData
     const { page = 1, limit = 100 } = req.query
-    const url = `https://api.github.com/repos/${user}/${repo}/pulls?state=open&per_page=${limit}&page=${page}`
+    const urlPRs = `https://api.github.com/repos/${user}/${repo}/pulls?state=open&per_page=${limit}&page=${page}`
 
-    const response = await fetch(url, { headers })
-    const githubResponseHeaders = response.headers.entries()
+    const responsePRs = await fetch(urlPRs, { headers })
+
+    const githubResponseHeaders = responsePRs.headers.entries()
     const headersForClient = []
     for (let header of githubResponseHeaders) {
       headersForClient.push(header)
     }
-    const json = await response.json()
-    res.json({ data: json, headers: headersForClient })
+    const jsonPRs = await responsePRs.json()
+    const commitPromises = jsonPRs.map(pr => {
+      const headers = new Headers
+      headers.set('X-GitHub-Api-Version', '2022-11-28')
+      headers.set('Accept', 'application/vnd.github+json')
+      headers.set('Authorization', `Basic ${authString}`)
+
+      const urlCommits = `https://api.github.com/repos/${user}/${repo}/pulls/${pr.number}/commits`
+      return fetch(urlCommits, { headers })
+    })
+
+    const responseCommits = await Promise.all(commitPromises)
+    const jsonCommits = await Promise.all(responseCommits.map(res => res.json()))
+
+    const dataForClient = jsonPRs.map(pr => ({
+      id: pr.id,
+      number: pr.number,
+      title: pr.title,
+      author: pr.user.login,
+      commits: jsonCommits,
+    }))
+    res.json(dataForClient)
   } catch (error) {
     next(error)
   }
