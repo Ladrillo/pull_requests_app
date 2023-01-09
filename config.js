67d8c91c363ac325a06350d06f8943d3beaaf344
Gabriel Cabrejas <gabriel@cabrejas.net>
2023-01-09 13:24:01 +0100

    wip

--- config.js
+++ config.js
@@ -1,13 +1,11 @@
 const username = process.env.USERNAME
 const token = process.env.ACCESS_TOKEN
-const authString = process.env.ACCESS_TOKEN ? Buffer.from(`${username}:${token}`).toString('base64') : ''
+const authString = token ? Buffer.from(`${username}:${token}`).toString('base64') : ''
+const origin = process.env.NODE_ENV === 'production' ? 'https://pr-app.herokuapp.com' : ''
 const port = process.env.PORT || 9000
-const origin = process.env.NODE_ENV === 'production' ? 'https://pr-app.herokuapp.com/' : ''
 
 module.exports = {
-  token,
-  username,
   authString,
-  port,
   origin,
+  port,
 }
